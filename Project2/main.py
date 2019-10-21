0d1ed2ab0fd7bb89268ae99531098632812c369d
zzkzzk1996 <zekunzhang.1996@gmail.com>
2019-10-21 18:57:02 -0400

    FlipWihtBayesian

--- Project2/main.py
+++ Project2/main.py
@@ -1,5 +1,7 @@
-import utils
+import sweeper
+import minemap
 import numpy as np
+import time
 
 # map_generator = utils.MineMap()
 # board = map_generator.board
@@ -7,32 +9,81 @@ import numpy as np
 # print(board)
 
 if __name__ == '__main__':
-    map_generator = utils.MineMap(x=20, y=20)
-    board = map_generator.board
-    # board = np.array([[-1, 1, 1, -1, -1, 2, 1, 1, 1, 1, ],
-    #                   [1, 2, 2, 3, 3, -1, 1, 2, -1, 2, ],
-    #                   [1, 2, -1, 1, 1, 1, 2, 3, -1, 2, ],
-    #                   [-1, 2, 1, 1, 0, 0, 1, -1, 2, 1, ],
-    #                   [1, 1, 1, 2, 2, 1, 1, 1, 1, 0, ],
-    #                   [0, 0, 2, -1, -1, 1, 0, 0, 0, 0, ],
-    #                   [0, 0, 2, -1, 3, 2, 1, 2, 1, 1, ],
-    #                   [0, 0, 2, 2, 2, 1, -1, 3, -1, 1, ],
-    #                   [1, 1, 1, -1, 3, 3, 3, -1, 3, 2, ],
-    #                   [-1, 1, 1, 2, -1, -1, 2, 1, 2, -1, ]])
-    # mine_number = 0
-    # row, column = np.shape(board)
-    # for i in range(row):
-    #     for j in range(column):
-    #         if board[i][j] == -1:
-    #             mine_number += 1
+    avg, avg_improve, time_con, time_con_improve = 0, 0, 0, 0
+    for i in range(100):
+        map_generator = minemap.MineMap(x=16, y=16)
+        board = map_generator.board
+        # board = np.array([[-1, 1, 1, -1, -1, 2, 1, 1, 1, 1, ],
+        #                   [1, 2, 2, 3, 3, -1, 1, 2, -1, 2, ],
+        #                   [1, 2, -1, 1, 1, 1, 2, 3, -1, 2, ],
+        #                   [-1, 2, 1, 1, 0, 0, 1, -1, 2, 1, ],
+        #                   [1, 1, 1, 2, 2, 1, 1, 1, 1, 0, ],
+        #                   [0, 0, 2, -1, -1, 1, 0, 0, 0, 0, ],
+        #                   [0, 0, 2, -1, 3, 2, 1, 2, 1, 1, ],
+        #                   [0, 0, 2, 2, 2, 1, -1, 3, -1, 1, ],
+        #                   [1, 1, 1, -1, 3, 3, 3, -1, 3, 2, ],
+        #                   [-1, 1, 1, 2, -1, -1, 2, 1, 2, -1, ]])
+        # mine_number = 0
+        # row, column = np.shape(board)
+        # for i in range(row):
+        #     for j in range(column):
+        #         if board[i][j] == -1:
+        #             mine_number += 1
+        mine = map_generator.mine_number
+        sweep = sweeper.Sweeper(board, map_generator.mine_number)
+        start = time.time()
+        while sweep.flip():
+            sweep.sweep_safe()
+            while sweep.sweep_mine(): continue
+        end = time.time()
+        time_con += end - start
+        t = sweep.temp / mine
+        avg += t
 
-    sweeper = utils.Sweeper(board, map_generator.mine_number)
-    while sweeper.flip():
-        sweeper.sweep_safe()
-        while sweeper.sweep_mine(): continue
+        sweep_improve = sweeper.Sweeper(board, map_generator.mine_number)
+        start_improve = time.time()
+        while sweep_improve.flip_by_possibility():
+            sweep_improve.sweep_safe()
+            while sweep_improve.sweep_mine(): continue
+        end_improve = time.time()
+        time_con_improve += end_improve - start_improve
+        t_improve = sweep_improve.temp / mine
+        avg_improve += t_improve
+        # sweeper.draw_board()
+        # map_generator = minemap.MineMap()
+        # map_generator.drawboard(board)
+        # print(sweeper.sweeper_map)
+        # print(board)
+    avg /= 100
+    avg_improve /= 100
+    print(str(avg) + " -> " + str(time_con))
+    print(str(avg_improve) + " -> " + str(time_con_improve))
 
-    sweeper.draw_board()
-    map_generator = utils.MineMap()
-    map_generator.drawboard(board)
-    print(sweeper.sweeper_map)
-    print(board)
+    # map_generator = minemap.MineMap(x=16, y=16)
+    # board = map_generator.board
+    # # board = np.array([[-1, 1, 1, -1, -1, 2, 1, 1, 1, 1, ],
+    # #                   [1, 2, 2, 3, 3, -1, 1, 2, -1, 2, ],
+    # #                   [1, 2, -1, 1, 1, 1, 2, 3, -1, 2, ],
+    # #                   [-1, 2, 1, 1, 0, 0, 1, -1, 2, 1, ],
+    # #                   [1, 1, 1, 2, 2, 1, 1, 1, 1, 0, ],
+    # #                   [0, 0, 2, -1, -1, 1, 0, 0, 0, 0, ],
+    # #                   [0, 0, 2, -1, 3, 2, 1, 2, 1, 1, ],
+    # #                   [0, 0, 2, 2, 2, 1, -1, 3, -1, 1, ],
+    # #                   [1, 1, 1, -1, 3, 3, 3, -1, 3, 2, ],
+    # #                   [-1, 1, 1, 2, -1, -1, 2, 1, 2, -1, ]])
+    # # mine_number = 0
+    # # row, column = np.shape(board)
+    # # for i in range(row):
+    # #     for j in range(column):
+    # #         if board[i][j] == -1:
+    # #             mine_number += 1
+    # mine = map_generator.mine_number
+    # sweep = sweeper.Sweeper(board, map_generator.mine_number)
+    # while sweep.flip_by_possibility():
+    #     sweep.sweep_safe()
+    #     while sweep.sweep_mine(): continue
+    # sweep.draw_board()
+    # map_generator = minemap.MineMap()
+    # map_generator.drawboard(board)
+    # print(sweep.sweeper_map)
+    # print(board)
--- Project2/sweeper.py
+++ Project2/sweeper.py
@@ -1,99 +1,22 @@
 import numpy as np
-# import random
-
 import matplotlib.pyplot as plt
 
 dir_arr = ([0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1])
 
 
-class MineMap:
-    """
-    class for generating mine map
-    """
-
-    def __init__(self, x=10, y=10, p=0.2):
-        self.x = 10
-        self.y = 10
-        self.mine_number = p * x * y if 1 > p > 0 else p
-        self.generate_board()
-
-    def generate_map(self):
-        """
-        :return: the mine map
-        generate mine map with given possibility of mine or given number of mines
-        """
-        x, y, mine_number = self.x, self.y, int(self.mine_number)
-        tmp_arr = np.zeros(x * y)
-        mines = np.random.choice(range(x * y), size=mine_number, replace=False)
-
-        for mine in mines:
-            tmp_arr[mine] = -1
-
-        map = tmp_arr.reshape(x, y)
-        return map
-
-    def generate_tips(self, board):
-        """
-        :param board: the mine map
-        generate the mine tips for player
-        :return the tipped mine map
-        """
-        x, y = board.shape[0], board.shape[1]
-        bigboard = np.zeros((x + 2, y + 2))  # generate a bigger map to avoid complex logical condition determination
-        bigboard[1:x + 1, 1:y + 1] = board
-        mine_positions = np.argwhere(bigboard == -1)
-        # mark the mine's adjacent position to 1, 2, 3 or so
-        for i, j in mine_positions:
-            mine = (i, j)
-            for dir in dir_arr:
-                m = mine[0] + dir[0]
-                n = mine[1] + dir[1]
-                if bigboard[m, n] == -1:
-                    continue
-                else:
-                    bigboard[m, n] += 1
-
-        board = bigboard[1:x + 1, 1:y + 1]
-        return board
-
-    def generate_board(self):
-        board = self.generate_map()
-        board = self.generate_tips(board)
-        self.board = board
-
-    def drawboard(self, originalboard=None):
-        if originalboard is None:
-            board = self.board.copy()
-        else:
-            board = originalboard.copy()
-        board[board == -1] = -5
-        board[board == -2] = -12
-        if (board != -2).all():
-            board[board == -1] = -2
-            # board[board>0]=11
-            board[board == 0] = 0
-
-        plt.figure(figsize=(5, 5))
-        plt.pcolor(-board[::-1], edgecolors='black', cmap='bwr', linewidths=2)
-        plt.xticks([]), plt.yticks([])
-        plt.tight_layout()
-        plt.show()
-
-
-# ms = MineMap(p=0.14)
-# print(ms.board)
-# ms.drawboard()
 class Sweeper:
     """
     this class is to generate the sweeper's map and the search algorithm
     """
 
     def __init__(self, board=None, mine_number=0):
+        self.temp = 0
         self.row, self.col = np.shape(board)
         self.mine_map = board
         self.sweeper_map = np.full((self.row, self.col), -1, dtype=int)
         self.mine_number = mine_number
         self.start_point = [0, 0]
+        self.possibility_map = np.full((self.row, self.col), 0, dtype=int)
 
     def sweep_safe(self):
         for i in range(self.start_point[0], self.row):
@@ -140,10 +63,19 @@ class Sweeper:
                 current_value = self.sweeper_map[i][j]
                 if current_value > 0:
                     if self.get_neighbor(current_value=current_value, current_location=(i, j)) != -1:
+                        # check_map = self.sweeper_map
                         if self.get_neighbor(current_value=current_value, current_location=(i, j)) == current_value:
                             self.set_mines(current_location=(i, j))
+                            # check = self.map_check(check_map)
                             check = True
         return check
+
+    def map_check(self, map):
+        for i in range(self.row):
+            for j in range(self.col):
+                if map[i][j] != self.sweeper_map[i][j]:
+                    return True
+        return False
 
     def set_mines(self, current_location=(0, 0)):
         i, j = current_location[0], current_location[1]
@@ -163,9 +95,71 @@ class Sweeper:
                 if self.sweeper_map[i][j] == -1:
                     if self.mine_map[i][j] == -1:
                         self.sweeper_map[i][j], self.mine_number = -3, self.mine_number - 1
+                        self.temp += 1
                     else:
                         self.sweeper_map[i][j] = self.mine_map[i][j]
                     return True
+
+    def flip_by_possibility(self):
+        if self.mine_number == 0:
+            return False
+        for i in range(self.row):
+            for j in range(self.col):
+                self.possibility_map[i][j] = 0
+        pos_i, pos_j, max_p = 0, 0, 1
+        for i in range(self.row):
+            for j in range(self.col):
+                self.add_possibility((i, j))
+                if self.possibility_map[i][j] < max_p:
+                    max_p, pos_i, pos_j = self.possibility_map[i][j], i, j
+        if pos_i == 0 and pos_j == 0:
+            return self.flip()
+        else:
+            if self.mine_map[pos_i][pos_j] == -1:
+                self.sweeper_map[pos_i][pos_j], self.mine_number = -3, self.mine_number - 1
+                self.temp += 1
+            else:
+                self.sweeper_map[pos_i][pos_j] = self.mine_map[pos_i][pos_j]
+            return True
+
+    def add_possibility(self, current_location=(0, 0)):
+        i, j = current_location[0], current_location[1]
+        unknown, mines = 0, self.sweeper_map[i][j]
+        if mines < 1: return  # if the current position is not a tip return
+        for dir in dir_arr:
+            point = [i + dir[0] if 0 <= i + dir[0] < self.row else -1,
+                     j + dir[1] if 0 <= j + dir[1] < self.col else -1]
+            if point[0] == -1 or point[1] == -1:
+                continue
+            if self.sweeper_map[point[0]][point[1]] == -1:
+                unknown += 1
+            elif self.sweeper_map[point[0]][point[1]] < -1:
+                mines -= 1
+        if unknown == 0: return  # if there's no mine or all mines have been spotted
+        if mines == unknown:
+            self.set_mines((i, j))
+        else:
+            for dir in dir_arr:
+                point = [i + dir[0] if 0 <= i + dir[0] < self.row else -1,
+                         j + dir[1] if 0 <= j + dir[1] < self.col else -1]
+                if point[0] == -1 or point[1] == -1:
+                    continue
+                self.possibility_map[point[0]][point[1]] += 1
+
+    def calculate_possibility(self, current_location=(0, 0)):
+        i, j, unknown, total = current_location[0], current_location[1], 0, 0
+        possibility = 100 if i == 0 and j == 0 else 0
+        for dir in dir_arr:
+            point = [i + dir[0] if 0 <= i + dir[0] < self.row else -1,
+                     j + dir[1] if 0 <= j + dir[1] < self.col else -1]
+
+            if point[0] == -1 or point[1] == -1:
+                continue
+            total += 1
+            if self.sweeper_map[point[0]][point[1]] == -1:
+                unknown += 1
+            elif self.sweeper_map[point[0]][point[1]] < -1:
+                total -= 1
 
     def draw_board(self):
         sweeper_map = self.sweeper_map.copy()
