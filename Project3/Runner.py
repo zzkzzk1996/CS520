b28ca1617616f87b46440b75fd1dbc92fd3cadff
Aaron723 <w745207699@gmail.com>
2019-11-10 21:34:14 -0500

    fix runner bugs

--- Project3/Runner.py
+++ Project3/Runner.py
@@ -7,9 +7,7 @@
 # @time: 2019-11-08 16:16:35
 
 import Map
-import Stationery
+
 if __name__ == '__main__':
     map = Map.Map(10)
     map.printer()
-    run = Stationery.Explorer(map)
-    run.search_one()
