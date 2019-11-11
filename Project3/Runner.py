f7a85d2f1feaaea7ce2ed084aee5b7fa4306a698
Zzk__ <43152228+zzkzzk1996@users.noreply.github.com>
2019-11-10 22:32:03 -0500

    Update Runner.py

--- Project3/Runner.py
+++ Project3/Runner.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# @project : Project3
+# @project : Assignment3
 # @author: Zekun Zhang
 # @contact: zekunzhang.1996@gmail.com
 # @file: Runner.py
 # @time: 2019-11-08 16:16:35
 
 import Map
+import Stationery
+import csv
+
+path = "/Users/zekunzhang/2019 Fall/CS520/Assignment3"
+filename1 = "/Rule_1.csv"
+filename2 = "/Rule_2.csv"
+
+
+def write_csv(info, filename):
+    csvFile = open(path + filename, "a")
+    writer = csv.writer(csvFile)
+    writer.writerow(info)
+    csvFile.close()
+
+
+def test(times):
+    res1, res2 = 0, 0
+    for i in range(times):
+        map = Map.Map(10)
+        se = Stationery.Explorer(origin_map=map)
+        res1 += se.search_one()
+        res2 += se.search_two()
+    # print("Average Search Times: " + str(res / times))
+    return res1, res2
+
 
 if __name__ == '__main__':
-    map = Map.Map(10)
-    map.printer()
+    for i in range(1, 1000):
+        res1, res2 = test(i)
+        info1 = [i, round(res1 / i)]
+        info2 = [i, round(res2 / i)]
+        write_csv(info1, filename1)
+        write_csv(info2, filename2)
+        print("Progress:{}%".format((i * 100 / 1000)), flush=True)
