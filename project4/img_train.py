7457d8bbcf27e509b128629b5bd70dadf3b8583a
Aaron723 <w745207699@gmail.com>
2020-01-03 01:41:24 -0500

    1.1 version of CNN, update activation functions, split the backpropagation of convolutional layer

--- project4/img_train.py
+++ project4/img_train.py
@@ -6,8 +6,21 @@
 import numpy as np
 
 
+def relu(x):
+    return np.maximum(x, 0)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+
 class NN:
-    def __init__(self, kernel_size=3, lr=1e-7, input_array=None, output_array=None, method="valid"):
+    def __init__(self, kernel_size=3, lr=1e-6, input_array=None, output_array=None, method="valid",
+                 activation_function='relu'):
         self.kernel_size = kernel_size
         self.learning_rate = lr
         self.input_array = input_array
@@ -15,6 +28,7 @@ class NN:
         self.weights = np.random.randn(kernel_size, kernel_size)
         self.stride = 1
         self.method = method
+        self.activation = activation_function
 
     def im2col(self, input_img, ksize, stride):
         # input_img is a 2d tensor([width ,height])
@@ -38,24 +52,48 @@ class NN:
         # conv_out = np.reshape(conv_out, np.hstack(self.input_array.shape))
         # the output of column format
         conv_out = np.reshape(conv_out_c, np.hstack((x.shape[0] - self.kernel_size + 1,
-                                                  x.shape[0] - self.kernel_size + 1)))
+                                                     x.shape[0] - self.kernel_size + 1)))
         return conv_out, conv_out_c
+
+    def back_conv(self, gradient_conv_c, conv_c):
+        grad_kernel_c = np.dot(gradient_conv_c, conv_c)
+        return np.reshape(grad_kernel_c, np.hstack(self.weights.shape))
 
     def train(self, epoches=100):
         for t in range(epoches):
             h, h_c = self.forward(self.input_array)
-            h_relu, h_c_relu = np.maximum(h, 0), np.maximum(h_c, 0)
-            # out put in column format
-            out_put_c = np.reshape(self.output_array, np.hstack(h_c_relu.shape))
-            loss, loss_c = np.square(h_relu - self.output_array).sum(),  np.square(h_c_relu - out_put_c).sum()
-            print(t, loss, loss_c)
-            grad_loss, grad_loss_c = 2 * (h_relu - self.output_array), 2 * (h_c_relu - out_put_c)
-            grad_h_relu, grad_h_c_relu = grad_loss.copy(), grad_loss_c.copy()
-            grad_h_relu[grad_h_relu < 0] = 0
-            grad_h_c_relu[grad_h_c_relu < 0] = 0
-            grad_kernel_c = np.dot(self.col_image.T, grad_h_c_relu)
-            grad_kernel = np.reshape(grad_kernel_c, np.hstack(self.weights.shape))
-            # grad_kernel = np.dot(np.reshape(self.col_image, np.hstack(self.input_array.shape)), grad_h_relu)
+            if self.activation == 'relu':
+                h_act, h_c_act = relu(h), relu(h_c)
+                # out put in column format
+                out_put_c = np.reshape(self.output_array, np.hstack(h_c_act.shape))
+                loss, loss_c = np.square(h_act - self.output_array).sum(), np.square(h_c_act - out_put_c).sum()
+                print(t, loss, loss_c)
+                grad_loss, grad_loss_c = 2 * (h_act - self.output_array), 2 * (h_c_act - out_put_c)
+                grad_h_act, grad_h_c_act = grad_loss.copy(), grad_loss_c.copy()
+                grad_h_act[grad_h_act < 0] = 0
+                grad_h_c_act[grad_h_c_act < 0] = 0
+            elif self.activation == 'sigmoid':
+                h_act, h_c_act = sigmoid(h), sigmoid(h_c)
+                # out put in column format
+                out_put_c = np.reshape(self.output_array, np.hstack(h_c_act.shape))
+                loss, loss_c = np.square(h_act - self.output_array).sum(), np.square(h_c_act - out_put_c).sum()
+                print(t, loss, loss_c)
+                grad_loss, grad_loss_c = 2 * (h_act - self.output_array), 2 * (h_c_act - out_put_c)
+                grad_h_act, grad_h_c_act = (sigmoid(grad_loss) * (1 - sigmoid(grad_loss))), (
+                        sigmoid(grad_loss_c) * (1 - sigmoid(grad_loss_c)))
+            elif self.activation == 'tanh':
+                h_act, h_c_act = tanh(h), tanh(h_c)
+                # out put in column format
+                out_put_c = np.reshape(self.output_array, np.hstack(h_c_act.shape))
+                loss, loss_c = np.square(h_act - self.output_array).sum(), np.square(h_c_act - out_put_c).sum()
+                print(t, loss, loss_c)
+                grad_loss, grad_loss_c = 2 * (h_act - self.output_array), 2 * (h_c_act - out_put_c)
+                grad_h_act, grad_h_c_act = (1 - tanh(grad_loss) * tanh(grad_loss)), (
+                        1 - tanh(grad_loss_c) * tanh(grad_loss_c))
+            # grad_kernel_c = np.dot(self.col_image.T, grad_h_c_act)
+            # grad_kernel = np.reshape(grad_kernel_c, np.hstack(self.weights.shape))
+            grad_kernel = self.back_conv(self.col_image.T, grad_h_c_act)
+            # grad_kernel = np.dot(np.reshape(self.col_image, np.hstack(self.input_array.shape)), grad_h_act)
             self.weights = self.weights - self.learning_rate * grad_kernel
 
     def predict(self, input_vector, output_vector):
@@ -65,10 +103,18 @@ class NN:
         print(loss)
 
 
+def normalization(x):
+    xmin, xmax = x.min(), x.max()
+    return (x - xmin) / (xmax - xmin)
+
+
 if __name__ == '__main__':
     train_input, train_output = np.random.randn(255, 255), np.random.randn(255, 255)
+    norm_in, norm_out = normalization(train_input), normalization(train_output)
     # train_input, train_output = np.random.randint(0, 255, (3, 3)), np.random.randint(0, 255, (2, 2))
     # nn = NN(input_array=train_input, output_array=train_output, kernel_size=2)
-    nn = NN(input_array=train_input, output_array=train_output, kernel_size=3, method='SAME')
-    nn.train(1600)
+    # nn = NN(input_array=train_input, output_array=train_output, kernel_size=3, method='SAME', lr=1e-7)
+    nn = NN(input_array=norm_in, output_array=norm_out, kernel_size=3, method='SAME', activation_function='tanh', lr=1e-7)
+    # nn = NN(input_array=train_input, output_array=train_output, kernel_size=3, method='SAME',activation_function='sigmoid', lr=1e-11)
+    nn.train(200)
     nn.predict(train_input, train_output)
